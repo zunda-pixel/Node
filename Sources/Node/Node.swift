e199f1045310859104d39051e2aae203fd481a81
zunda <zunda.pixel@gmail.com>
2022-10-24 18:05:07 +0900

    Nodeを実装

--- Sources/Node/Node.swift
+++ Sources/Node/Node.swift
@@ -1,6 +1,29 @@
-public struct Node {
-    public private(set) var text = "Hello, World!"
+public protocol Node {
+  associatedtype ID: Equatable
+  associatedtype Source: NodeSource where Source.ID == ID
+  
+  var id: ID { get }
+  var children: [Self] { get set }
+  
+  init(id: ID)
+}
 
-    public init() {
+public extension Node {
+  mutating func setChildren(sources: [Source]) {
+    let foundData = sources.filter { id == $0.parentID }
+    children = foundData.map { .init(id: $0.id) }
+  }
+
+  mutating func setData(sources: [Source]) {
+    for i in children.indices {
+      children[i].setChildren(sources: sources)
+
+      children[i].setData(sources: sources)
     }
+  }
+
+  mutating func setAllData(sources: [Source]) {
+    setChildren(sources: sources)
+    setData(sources: sources)
+  }
 }
