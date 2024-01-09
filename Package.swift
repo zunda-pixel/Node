c98d195064d8412a1d6c0f6efbb4fd2b692cdb48
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-01-10 01:14:24 +0900

    remove .swiftpm

--- Package.swift
+++ Package.swift
@@ -4,25 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "Node",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Node",
-            targets: ["Node"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Node",
-            dependencies: []),
-        .testTarget(
-            name: "NodeTests",
-            dependencies: ["Node"]),
-    ]
+  name: "Node",
+  products: [
+    .library(
+      name: "Node",
+      targets: ["Node"]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "Node"
+    ),
+    .testTarget(
+      name: "NodeTests",
+      dependencies: ["Node"]
+    ),
+  ]
 )
